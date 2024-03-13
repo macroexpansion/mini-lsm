6442f826c70fdbd673bc0bf97cad779ea2b88f31
macroexpansion <trasuadev@gmail.com>
2024-03-13 21:39:24 +0700

    week2_day1: task1

--- mini-lsm-starter/src/compact.rs
+++ mini-lsm-starter/src/compact.rs
@@ -4,6 +4,7 @@ mod leveled;
 mod simple_leveled;
 mod tiered;
 
+use std::collections::HashSet;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -15,8 +16,10 @@ pub use simple_leveled::{
 };
 pub use tiered::{TieredCompactionController, TieredCompactionOptions, TieredCompactionTask};
 
+use crate::iterators::merge_iterator::MergeIterator;
+use crate::iterators::StorageIterator;
 use crate::lsm_storage::{LsmStorageInner, LsmStorageState};
-use crate::table::SsTable;
+use crate::table::{SsTable, SsTableBuilder, SsTableIterator};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum CompactionTask {
@@ -107,12 +110,136 @@ pub enum CompactionOptions {
 }
 
 impl LsmStorageInner {
-    fn compact(&self, _task: &CompactionTask) -> Result<Vec<Arc<SsTable>>> {
-        unimplemented!()
+    fn compact(&self, task: &CompactionTask) -> Result<Vec<Arc<SsTable>>> {
+        if let CompactionTask::ForceFullCompaction {
+            l0_sstables,
+            l1_sstables,
+        } = task
+        {
+            let snapshot = {
+                let guard = self.state.read();
+                Arc::clone(&guard)
+            }; // drop global lock here
+
+            let mut sstable_iters = Vec::with_capacity(l0_sstables.len() + l1_sstables.len());
+            for sstable_id in l0_sstables.iter().chain(l1_sstables.iter()) {
+                let sstable = snapshot.sstables.get(sstable_id).unwrap().clone();
+                sstable_iters.push(Box::new(SsTableIterator::create_and_seek_to_first(
+                    sstable,
+                )?));
+            }
+
+            let mut sstable_iter = MergeIterator::create(sstable_iters);
+
+            // iterate over sstables, take the latest key-value pair and put it into new sstables
+            // all newly created sstables has non-overlap key range
+            let mut sst_builder = None;
+            let mut new_sstables = Vec::new();
+            while sstable_iter.is_valid() {
+                if sst_builder.is_none() {
+                    sst_builder = Some(SsTableBuilder::new(self.options.block_size));
+                }
+                let sst_builder_inner = sst_builder.as_mut().unwrap();
+                if !sstable_iter.value().is_empty() {
+                    sst_builder_inner.add(sstable_iter.key(), sstable_iter.value());
+                }
+                sstable_iter.next()?;
+
+                // create a new sstable if exceeds target_sst_size
+                if sst_builder_inner.estimated_size() >= self.options.target_sst_size {
+                    let new_sst_id = self.next_sst_id();
+                    let sst_builder = sst_builder.take().unwrap();
+                    let sst = Arc::new(sst_builder.build(
+                        new_sst_id,
+                        Some(self.block_cache.clone()),
+                        self.path_of_sst(new_sst_id),
+                    )?);
+                    new_sstables.push(sst);
+                }
+            }
+
+            // create a new sstable
+            if let Some(builder) = sst_builder {
+                let sst_id = self.next_sst_id();
+                let sstable = Arc::new(builder.build(
+                    sst_id,
+                    Some(self.block_cache.clone()),
+                    self.path_of_sst(sst_id),
+                )?);
+                new_sstables.push(sstable);
+            }
+
+            return Ok(new_sstables);
+        }
+
+        todo!()
     }
 
     pub fn force_full_compaction(&self) -> Result<()> {
-        unimplemented!()
+        let CompactionOptions::NoCompaction = self.options.compaction_options else {
+            panic!("full compaction can only be called with compaction is not enabled")
+        };
+
+        let snapshot = {
+            let state = self.state.read();
+            state.clone()
+        };
+
+        let l0_sstables = snapshot.l0_sstables.clone();
+        let l1_sstables = snapshot.levels[0].1.clone();
+        let compaction_task = CompactionTask::ForceFullCompaction {
+            l0_sstables: l0_sstables.clone(),
+            l1_sstables: l1_sstables.clone(),
+        };
+
+        println!("force full compaction: {:?}", compaction_task);
+
+        let sstables = self.compact(&compaction_task)?;
+        let mut ids = Vec::with_capacity(sstables.len());
+
+        {
+            let _state_lock = self.state_lock.lock();
+            let mut state = self.state.read().as_ref().clone();
+
+            // remove old sstables
+            for sst in l0_sstables.iter().chain(l1_sstables.iter()) {
+                let result = state.sstables.remove(sst);
+                assert!(result.is_some());
+            }
+
+            // insert new sorted sstables
+            for new_sst in sstables {
+                ids.push(new_sst.sst_id());
+                let result = state.sstables.insert(new_sst.sst_id(), new_sst);
+                assert!(result.is_none());
+            }
+            assert_eq!(l1_sstables, state.levels[0].1);
+
+            // assign all new sstable ids to L1 sstables
+            state.levels[0].1 = ids.clone();
+
+            // remove old sstable ids from L0 sstables
+            let mut l0_sstables_map = l0_sstables.iter().copied().collect::<HashSet<_>>();
+            state.l0_sstables = state
+                .l0_sstables
+                .iter()
+                .filter(|x| !l0_sstables_map.remove(x))
+                .copied()
+                .collect::<Vec<_>>();
+            assert!(l0_sstables_map.is_empty());
+
+            // update new state
+            *self.state.write() = Arc::new(state);
+        }
+
+        // remove existing sst file
+        for sst in l0_sstables.iter().chain(l1_sstables.iter()) {
+            std::fs::remove_file(self.path_of_sst(*sst))?;
+        }
+
+        println!("force full compaction done, new SSTs: {:?}", ids);
+
+        Ok(())
     }
 
     fn trigger_compaction(&self) -> Result<()> {
